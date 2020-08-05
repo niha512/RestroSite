a55889e9d3f5ce0e36848abd51295f4c2aa00f1b
Niharika Gorantla <niharikagorantla@Niharikas-MacBook-Air.local>
2020-08-05 19:54:16 +0530

    initial setup

---
+++ backend/src/main/java/com/site/restro/RestroApplication.java
@@ -0,0 +1,13 @@
+package com.site.restro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestroApplication.class, args);
+	}
+
+}
